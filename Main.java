bb4f6ff6155df2ff28da38a815884a435b7e4fb9
LGordy20 <liam.gordy@patelhs.org>
2021-01-29 17:21:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void mnain(String[] args) {
+    Game g1 = new Game("forza",-40);
+    Game g2 = new Game("Call of Duty",-60);
+    Game g3 = new Game("Rocket League",-10);
+
+    System.out.println("If you are looking for a cheap game you should buy" );
+  }
+}
