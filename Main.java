547b5509da2ee97cc529e8e9f96f2419bfc8db8d
LGordy20 <liam.gordy@patelhs.org>
2021-01-29 17:27:45 +0000

    I added a print statement

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ class Main {
     Game g2 = new Game("Call of Duty",-60);
     Game g3 = new Game("Rocket League",-10);
 
-    System.out.println("If you are looking for a cheap game you should buy" );
+    System.out.println("How much money are you looking to spend today?");
   }
 }
