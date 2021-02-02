2e4d1a66c7d3cef8c360642dbd0a77e989260889
LGordy20 <liam.gordy@patelhs.org>
2021-02-02 15:13:13 +0000

    Had my string names receive values and added a rint statment

--- Main.java
+++ Main.java
@@ -2,13 +2,20 @@ class Main {
   public static void main(String[] args) {
     
   
-    Game g1 = new Game("forza",-40);
-    Game g2 = new Game("Call of Duty",-60);
-    Game g3 = new Game("Rocket League",-10);
-
+    Game g1 = new Game("forza");
+    Game g2 = new Game("Call of Duty");
+    Game g3 = new Game("Rocket League");
+    
+     g1 = 40;
+     g2 = 60;
+     g3 = 10;
+    
     System.out.println("How much money are you looking to spend today?");
+
     System.out.println("I only can spend 10 dollars");
-    System.out.println(g3);
+
+    System.out.println("Then you should buy" + g3);
+
     
   }
 }
