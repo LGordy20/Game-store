b09980a253e458ed4ec5732657e22703beb48699
LGordy20 <liam.gordy@patelhs.org>
2021-02-02 14:40:06 +0000

    added

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 class Main {
-  public static void mnain(String[] args) {
+  public static void main(String[] args) {
+    
+  
     Game g1 = new Game("forza",-40);
     Game g2 = new Game("Call of Duty",-60);
     Game g3 = new Game("Rocket League",-10);
 
     System.out.println("How much money are you looking to spend today?");
+    System.out.println("I only can spend 10 dollars");
+    System.out.println(g3);
+    
   }
 }
