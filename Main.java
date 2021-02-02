23d01d6d4ba8742f0db206d133a7083d87e3dda1
LGordy20 <liam.gordy@patelhs.org>
2021-02-02 15:28:19 +0000

    put everything in one print statement

--- Main.java
+++ Main.java
@@ -1,21 +1,15 @@
 class Main {
   public static void main(String[] args) {
     
-  
-    Game g1 = new Game("forza");
-    Game g2 = new Game("Call of Duty");
-    Game g3 = new Game("Rocket League");
-    
-     g1 = 40;
-     g2 = 60;
-     g3 = 10;
+    System.out.println("Our top selling games are 40-forza, 60-Call of Duty, Rocket League. ");
+
     
     System.out.println("How much money are you looking to spend today?");
 
     System.out.println("I only can spend 10 dollars");
 
-    System.out.println("Then you should buy" + g3);
+    System.out.println("Then you should buy Rocket League. It is 10 dollars");
 
-    
+
   }
 }
